136e7ea67cdd51e8c0e4879de89806b89a30ef9b
BrendanBuono <brendan.buono@gmail.com>
2021-10-08 14:26:04 -0400

    Adding Halo bundle, better error handling

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
 import { sleep } from "https://deno.land/x/sleep/mod.ts";
 import {
   BestBuyStockChecker,
+  BestBuyHaloInfiniteStockChecker,
+  MicrosoftHaloInfiniteStockChecker,
   MicrosoftStockChecker,
   StockChecker,
 } from "./StockChecker.ts";
@@ -18,6 +20,8 @@ import {
 const stockCheckers: StockChecker[] = [
   new MicrosoftStockChecker(),
   new BestBuyStockChecker(),
+  new MicrosoftHaloInfiniteStockChecker(),
+  new BestBuyHaloInfiniteStockChecker(),
 ];
 
 const minWaitTime = 15;
@@ -36,16 +40,20 @@ const notificationEngine = new NotificationEngine(notifationMapper);
 while (true) {
   console.log(`checking stock at ${format(new Date(), "yyyy-MM-dd HH:mm:ss")}`);
 
-  for (let i = 0; i < stockCheckers.length; i++) {
-    const checker = stockCheckers[i];
-    console.log(`Checking stock for ${checker.siteName}`);
-    if (await checker.checkStock()) {
-      await notificationEngine.notify(
-        NotificationType.XSX,
-        `Xbox Series X available at ${checker.siteName}`,
-        `URL: ${checker.url}`,
-      );
+  try {
+    for (let i = 0; i < stockCheckers.length; i++) {
+      const checker = stockCheckers[i];
+      console.log(`Checking stock for ${checker.siteName}`);
+      if (await checker.checkStock()) {
+        await notificationEngine.notify(
+          NotificationType.XSX,
+          `${checker.notitifcationText} at ${checker.siteName}: `,
+          `${checker.url}`,
+        );
+      }
     }
+  } catch {
+    console.log("something went wrong, trying again");
   }
   const randomSleep = minWaitTime + Math.ceil(Math.random() * maxWaitDelta);
   console.log(`Waiting ${randomSleep} seconds to check again`);
--- stockChecker.ts
+++ stockChecker.ts
@@ -9,7 +9,7 @@ export interface StockChecker {
 
 export class MicrosoftStockChecker implements StockChecker {
   siteName = "Microsoft";
-  notitifcationText = "Series X is available on microsoft.com";
+  notitifcationText = "Series X is available";
   url = "https://www.xbox.com/en-us/configure/8wj714n3rbtl";
   lastInStockCheck: Date | undefined = undefined;
   async checkStock(): Promise<boolean> {
@@ -32,9 +32,68 @@ export class MicrosoftStockChecker implements StockChecker {
   }
 }
 
+export class MicrosoftHaloInfiniteStockChecker implements StockChecker {
+  siteName = "Microsoft (Halo)";
+  notitifcationText = "Halo Infinite Series X is available";
+  url = "https://www.xbox.com/en-us/configure/8RPM8T9CK0P6";
+  lastInStockCheck: Date | undefined = undefined;
+  async checkStock(): Promise<boolean> {
+    if (this.lastInStockCheck) {
+      const lastSuccessfulCheck = difference(
+        this.lastInStockCheck,
+        new Date(Date.now()),
+      );
+      if (lastSuccessfulCheck.minutes && lastSuccessfulCheck.minutes < 5) {
+        return Promise.resolve(false);
+      }
+    }
+    const results = await firefoxFetch(this.url, "www.xbox.com");
+    const body = await results.text();
+    const isInStock = !body.includes("Out of stock");
+    if (isInStock) {
+      this.lastInStockCheck = new Date(Date.now());
+    }
+    return isInStock;
+  }
+}
+
+
+export class BestBuyHaloInfiniteStockChecker implements StockChecker {
+  siteName = "Best Buy (Halo)";
+  notitifcationText = "Halo Infinite Series X is available";
+  url =
+  'https://www.bestbuy.com/site/microsoft-xbox-series-x-halo-infinite-limited-edition-black/6477938.p?skuId=6477938';
+  lastInStockCheck: Date | undefined = undefined;
+
+  async checkStock(): Promise<boolean> {
+    if (this.lastInStockCheck) {
+      const lastSuccessfulCheck = difference(
+        this.lastInStockCheck,
+        new Date(Date.now()),
+      );
+      if (lastSuccessfulCheck.minutes && lastSuccessfulCheck.minutes < 5) {
+        return Promise.resolve(false);
+      }
+    }
+    try {
+      const results = await firefoxFetch(this.url, "www.bestbuy.com");
+      const body = await results.text();
+      const isInStock = !body.includes('data-button-state="COMING_SOON"') &&
+        !body.includes("We're sorry, something went wrong");
+      if (isInStock) {
+        this.lastInStockCheck = new Date(Date.now());
+      }
+      return isInStock;
+    } catch {
+      return Promise.resolve(false);
+    }
+  }
+}
+
+
 export class BestBuyStockChecker implements StockChecker {
   siteName = "Best Buy";
-  notitifcationText = "Series X is available on bestbuy.com";
+  notitifcationText = "Series X is available";
   url =
     "https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324";
   lastInStockCheck: Date | undefined = undefined;
@@ -49,13 +108,17 @@ export class BestBuyStockChecker implements StockChecker {
         return Promise.resolve(false);
       }
     }
-    const results = await firefoxFetch(this.url, "www.bestbuy.com");
-    const body = await results.text();
-    const isInStock = !body.includes('data-button-state="SOLD_OUT"') &&
-      !body.includes("We're sorry, something went wrong");
-    if (isInStock) {
-      this.lastInStockCheck = new Date(Date.now());
+    try {
+      const results = await firefoxFetch(this.url, "www.bestbuy.com");
+      const body = await results.text();
+      const isInStock = !body.includes('data-button-state="SOLD_OUT"') &&
+        !body.includes("We're sorry, something went wrong");
+      if (isInStock) {
+        this.lastInStockCheck = new Date(Date.now());
+      }
+      return isInStock;
+    } catch {
+      return Promise.resolve(false);
     }
-    return isInStock;
   }
 }
